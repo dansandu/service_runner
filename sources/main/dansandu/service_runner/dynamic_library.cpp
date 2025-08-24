1e81f9548826caa9d4761f678e18b8ba0d96ff20
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2025-08-24 17:54:56 +0300

    Add library load logs

--- sources/main/dansandu/service_runner/dynamic_library.cpp
+++ sources/main/dansandu/service_runner/dynamic_library.cpp
@@ -1,4 +1,5 @@
 #include "dansandu/service_runner/dynamic_library.hpp"
+#include "dansandu/journey/logging.hpp"
 
 #include <string>
 
@@ -12,19 +13,22 @@ namespace dansandu::service_runner::dynamic_library
 #ifdef _WIN32
 struct Implementation
 {
-    explicit Implementation(const std::wstring& filePath) : library{LoadLibrary(filePath.c_str())}
+    explicit Implementation(const std::wstring& filePath) : filePath{filePath}, library{LoadLibrary(filePath.c_str())}
     {
         if (library == NULL)
         {
             throw CannotLoadLibraryException{L"Couldn't load library at path '" + filePath + L"'"};
         }
+        LOG_INFO("Library '", filePath, "' was loaded");
     }
 
     ~Implementation() noexcept
     {
+        LOG_INFO("Unloading library '", filePath, "'");
         FreeLibrary(library);
     }
 
+    std::wstring filePath;
     HMODULE library;
 };
 #endif
